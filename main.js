482f9819bc9f602349f4870262291fe907ec4213
Bartosz Utratny <utratny.bartosz@gmail.com>
2023-06-15 01:56:45 +0200

    Removed a forgotten comment

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-    // console.log(data[0].grade))
-function getRandomInt(min, max) {
+ function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
